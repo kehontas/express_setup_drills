2e551f44cc341740fd0b652d92b54aa16d6c2833
kehontas <kehontas@gmail.com>
2015-12-02 18:37:34 -0800

    move repo

---
+++ express-drill/base.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+
+var colors = [
+	"red",
+	"blue",
+	"green"
+];
+
+var fruits = [
+	"apples",
+	"banana",
+	"orange"
+];
+
+app.get('/', function (req, res){
+	res.send('Hello World!');
+	//res.sendFile('views/index.html' , { root : __dirname});
+});
+
+app.get("/colors", function (req, res){
+	res.send(colors.join(","));
+}); 
+
+app.get("/fruits", function (req, res){
+	res.send(fruits.join(","));
+});
+
+app.listen(process.env.PORT || 3000, function() {
+	console.log('Example app listening at http://localhost:3000/');
+});
